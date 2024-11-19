a720d9d63941cc65dfd6eb829c557f05857b1643
Erlzo <ericromerogasol53@gmail.com>
2024-11-19 16:39:57 +0100

    1
    -Declaro las palabras que tendremos en un array.
    -Declaro el "_" como letra que oculta.
    -Declaro los intentos restantes.
    -Declaro el bucle para cambiar las letras por el caracter ocultativo.

---
+++ src/juego_v4/Juego_Colgado.java
@@ -0,0 +1,52 @@
+package juego_v4;
+
+import java.util.Scanner;
+
+public class Juego_Colgado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		String[] palabras = {"alemania","polivalente","impresionante","estupefacientes"}; //palabras del juego
+		
+		System.out.println("Elije una de las palabras ocultas:");
+		int opcion = scanner.nextInt()-1; // scanner con la opcion a elejir de nuestra palabra oculta
+		
+		String palabraOculta = palabras[opcion]; //esta sera la palabra con la posicion						
+		
+		char letraOculta = '_'; // caracter para esconder las letras de las palabras
+		int intentosRestantes = 6;
+		StringBuilder palabraOcultaCompleta = new StringBuilder(); //declaracion del String	Builder para permititr modificaciones en las palabras
+		
+		//cambio de letras de las palabras por caracter "_"
+	       for (int i = 0; i < palabraOculta.length(); i++) { 
+	           palabraOcultaCompleta.append('_');
+	        }
+		
+		System.out.println("tas:" + palabraOculta);
+		System.out.println("tas:" + palabraOcultaCompleta);
+
+		
+	}
+
+}
+
+/*
+
+Reglas del juego:
+
+	1. El juego comienza pidiendo al administrador (un jugador) que elija una palabra secreta, la cual debe ser oculta durante el juego.
+	2. Cada jugador tiene que adivinar una letra de la palabra secreta en cada ronda.
+	3. El número máximo de intentos por jugador es 6.
+	4. Si un jugador adivina correctamente una letra, esta se revela en su posición en la palabra secreta.
+	5. Si un jugador se queda sin intentos, pierde la ronda.
+	6. El jugador que adivine toda la palabra primero o quien gane más rondas será el ganador.
+	
+Instrucciones:
+
+	1. El programa preguntar cuantos jugadores quieren jugar. El mínimo de jugadores tiene que ser 2.
+	2. El programa preguntará cuantas rondas se quieren jugar.
+	3. Para cada ronda, el programa debe:
+		· Mostrar las letras acertadas de la palabra secreta (con guiones bajos para las letras no adivinadas).
