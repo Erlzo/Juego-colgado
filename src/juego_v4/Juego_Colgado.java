ee8e001d9ebdfad42e71f944ddfb3272dd19997e
Erlzo <ericromerogasol53@gmail.com>
2024-12-01 21:54:00 +0100

    8
    - Codigo mas limpio

--- src/juego_v4/Juego_Colgado.java
+++ src/juego_v4/Juego_Colgado.java
@@ -11,11 +11,9 @@ public class Juego_Colgado {
 		
 		String[] palabras = {"alemania","polivalente","impresionante","estupefacientes"}; //palabras del juego
 		
-		int[] cuantosJugadores; // se pide los jugadores que van a jugar	
 		System.out.println("¿Cuantos jugadores juegan? | Mínimo 2 jugadores.");
 		int numJugadores = scanner.nextInt();
-		cuantosJugadores = new int[numJugadores];
-		
+		int[] cuantosJugadores = new int[numJugadores];
 		
 		System.out.println("¿Cuantas rondas jugaran?"); // se pide las rondas que quieren jugar
 		int rondas = scanner.nextInt();
@@ -23,14 +21,19 @@ public class Juego_Colgado {
 		int rondasGanadas = 0;
 		int rondasPerdidas = 0;
 		
-		
 		for(int k = 0; k < rondas; k++) {
 				if(k > 0) {
 					System.out.println("\n¡Nueva ronda!");
 				}
 
-			System.out.println("Elije una de las palabras ocultas:");
+			System.out.println("Elije una de las palabras ocultas: (1) (2) (3) (4)");
 			int opcion = scanner.nextInt()-1; // scanner con la opcion a elejir de nuestra palabra oculta
+			
+			if(opcion < 0 || opcion > 4) {
+				System.out.println("Error:");
+				System.out.println("Elije una de las palabras indicadas. (1) (2) (3) (4)");
+				break;
+			}
 			
 			String palabraOculta = palabras[opcion]; //esta sera la palabra con la posicion						
 			
@@ -39,19 +42,16 @@ public class Juego_Colgado {
 			int letrasPalabraOculta = palabraOculta.length();
 			
 			char [] palabraGuiones = new char[letrasPalabraOculta];
-			
-				
+							
 			for (int i = 0; i < palabraGuiones.length; i++) { 
 				palabraGuiones[i] = '_'; // se sustituye las letras por "_"
 		    }
 			  
-			
 			while (intentosRestantes > 0 && new String(palabraGuiones).contains("_")) { //bucle se ejecuta mientras queden intentos y no se haya completado la palabra / no hayan mas "_"
 		        System.out.println("Intentos restantes: " + intentosRestantes); // muestra intentos restantes
 		        System.out.println("Palabra actual: " + new String(palabraGuiones)); // muestra la palabra oculta
 		        System.out.print("Introduce una letra: "); // texto pidiendo una letra
-		        
-		        
+		        		        
 		        // Leer una letra
 		        
 		        char letraAdivinada = scanner.next().toLowerCase().charAt(0);// scanner pide la letra, este la pasa a minuscula por defecto, se escogera el caracter en posicion 0 es decir 'abc' = 'a'
@@ -72,7 +72,7 @@ public class Juego_Colgado {
 		        	        
 		        // Actualizar intentos o continuar
 		        
-		        if (letraEncontrada) {
+		        if (letraEncontrada == true) {
 		            System.out.println("¡Bien! La letra '" + letraAdivinada + "' está en la palabra."); // si esta la letra = mensaje felicitando
 		        } else {
 		            intentosRestantes--;
@@ -90,46 +90,12 @@ public class Juego_Colgado {
 		    	rondasPerdidas++;
 		        System.out.println("\nLo siento, te has quedado sin intentos. Has perdido.");
 		        System.out.println("\nLa palabra era: " + palabraOculta);
-
-
 		    }
 		    
-		    if(k == rondas) {
-		        System.out.println("");
-		    } else {
-		        System.out.println("\nRONDAS");
-		        System.out.println("\nWin " + rondasGanadas);
-		        System.out.println("\nLose " + rondasPerdidas);
+		    if(k == rondas || k < rondas ) {
+		        System.out.println("\nRONDAS = Win: " + rondasGanadas + " | Lose: " + rondasPerdidas);
 	        }
 		}
 		
 	}
 }
-
-
-
-/*
-
-Reglas del juego:
-
-si						1. El juego comienza pidiendo al administrador (un jugador) que elija una palabra secreta, la cual debe ser oculta durante el juego.
-si | faltan jugadores	2. Cada jugador tiene que adivinar una letra de la palabra secreta en cada ronda.
-si						3. El número máximo de intentos por jugador es 6.
-si						4. Si un jugador adivina correctamente una letra, esta se revela en su posición en la palabra secreta.
-si						5. Si un jugador se queda sin intentos, pierde la ronda.
-falta					6. El jugador que adivine toda la palabra primero o quien gane más rondas será el ganador.
-	
-Instrucciones:
-
-si	1. El programa preguntar cuantos jugadores quieren jugar. El mínimo de jugadores tiene que ser 2.
-si	2. El programa preguntará cuantas rondas se quieren jugar.
-	3. Para cada ronda, el programa debe:
-si		· Mostrar las letras acertadas de la palabra secreta (con guiones bajos para las letras no adivinadas).
-si		· Solicitar a los jugadores que ingresen una letra.
-si		· Comprobar si la letra ingresada es correcta.
-si		· Actualizar la palabra secreta con las letras adivinadas correctamente.
-si		· Contabilizar los intentos restantes para cada jugador.
-si		· Mostrar el estado de la palabra secreta después de cada intento.
-falta		· Al finalizar las rondas, mostrar el nombre del jugador que haya ganado más rondas.
-
-*/
